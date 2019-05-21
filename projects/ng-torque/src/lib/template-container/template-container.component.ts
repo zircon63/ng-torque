4be90cf0264bc5dc9734769d418f32a5bdc54539
zircon63 <canek1702@gmail.com>
2019-05-21 16:09:22 +0300

    refactor(template-container): set map type token

--- projects/ng-torque/src/lib/template-container/template-container.component.ts
+++ projects/ng-torque/src/lib/template-container/template-container.component.ts
@@ -10,23 +10,20 @@ export type MapType<T = any> = Map<string, Type<T>>;
 export const TEMPLATE_CONTAINER = new InjectionToken<MapTypeItem<any>>('TEMPLATE_CONTAINER');
 export const MAP_TYPE_TEMPLATE = new InjectionToken<Map<string, Type<any>>>('MAP_TYPE_TEMPLATE');
 
-export function provideTemplateContainer(values: MapTypeItem<any>[]) {
-  const templateProviders = values.map(value => {
-    return {
-      provide: TEMPLATE_CONTAINER,
-      useValue: value,
-      multi: true
-    };
-  });
-  const providers = [
-    ...templateProviders,
-    {
-      provide: MAP_TYPE_TEMPLATE,
-      useFactory: mapTypeFactory,
-      deps: [TEMPLATE_CONTAINER]
-    }
-  ];
-  return providers;
+export function provideTemplateContainer(value: MapTypeItem<any>) {
+  return {
+    provide: TEMPLATE_CONTAINER,
+    useValue: value,
+    multi: true
+  };
+}
+
+export function provideMapTypeTemplate() {
+  return {
+    provide: MAP_TYPE_TEMPLATE,
+    useFactory: mapTypeFactory,
+    deps: [TEMPLATE_CONTAINER]
+  };
 }
 
 export function mapTypeFactory(containers: MapTypeItem<any>[]) {
