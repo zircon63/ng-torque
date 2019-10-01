7583c6b5c66ed1f5de8a49c8feded33a546ec5a7
Zircon63 <canek1702@gmail.com>
2019-10-01 17:23:26 +0300

    fix(exports): add export types for new directives

--- projects/ng-torque/src/lib/ng-torque.module.ts
+++ projects/ng-torque/src/lib/ng-torque.module.ts
@@ -2,8 +2,6 @@ import {NgModule} from '@angular/core';
 import {CallOutputPipe} from './call-output.pipe';
 import {ComponentResolverDirective} from './directives/component-resolver.directive';
 import {DynamicControlDirective} from './directives/dynamic-control.directive';
-import {NgTorDynamicComponentDirective} from './directives/ng-tor-dynamic-component.directive';
-import {NgTorDynamicControlDirective} from './directives/ng-tor-dynamic-control.directive';
 import {NgTorDynamicTemplateDirective} from './directives/ng-tor-dynamic-template.directive';
 import {ViewQueryDirective} from './directives/view-query.directive';
 import {DynamicWrapperComponent} from './dynamic-wrapper/dynamic-wrapper.component';
@@ -13,9 +11,7 @@ const DIRECTIVES = [
   ComponentResolverDirective,
   ViewQueryDirective,
   DynamicControlDirective,
-  NgTorDynamicComponentDirective,
   NgTorDynamicTemplateDirective,
-  NgTorDynamicControlDirective
 ];
 const PIPES = [
   CallOutputPipe
@@ -32,7 +28,9 @@ const COMPONENTS = [
 export class NgTorqueModule {
 }
 
-export {NgTorDynamicComponentDirective} from './directives/ng-tor-dynamic-component.directive';
-export {NgTorDynamicControlDirective} from './directives/ng-tor-dynamic-control.directive';
 export {NgTorDynamicTemplateDirective} from './directives/ng-tor-dynamic-template.directive';
 export {CallOutputPipe} from './call-output.pipe';
+export {ViewQueryDirective} from './directives/view-query.directive';
+export {DynamicControlDirective} from './directives/dynamic-control.directive';
+export {DynamicWrapperComponent} from './dynamic-wrapper/dynamic-wrapper.component';
+export {ComponentResolverDirective} from './directives/component-resolver.directive';
